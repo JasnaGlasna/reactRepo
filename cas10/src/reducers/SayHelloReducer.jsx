3c5197347871ee5b6d1398af95719f9c8a5546d3
JasnaGlasna <113995549+JasnaGlasna@users.noreply.github.com>
2023-10-13 14:02:30 +0200

    domashna10

---
+++ cas10/src/reducers/SayHelloReducer.jsx
@@ -0,0 +1,22 @@
+const initialState = {
+  greeting: "Hi!"
+};
+
+const SayHelloReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SAY_HELLO":
+      return {
+        ...state,
+        greeting: action.payload
+      };
+    case "SAY_GOODBAY":
+      return {
+        ...state,
+        goodnight: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default SayHelloReducer;
