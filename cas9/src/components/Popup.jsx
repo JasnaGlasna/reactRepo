5f8ae7143065cf75255ca2c4172613358be29f7f
JasnaGlasna <113995549+JasnaGlasna@users.noreply.github.com>
2023-10-17 15:29:02 +0200

    Domashni

--- cas9/src/components/Popup.jsx
+++ cas9/src/components/Popup.jsx
@@ -1,27 +1,20 @@
-// import PropTypes from 'prop-types';
 import { useContext } from 'react';
 import { GalleryContext } from '../utils/GalleryContext';
 
-export const Popup = (
-    // {zatvoriSlika,izbranaSlika}
-    ) =>{
-        const {selectedImage,setSelectedImage, deleteImage} = useContext(GalleryContext);
-    return(
+export const Popup = () => {
+    const { selectedImage, setSelectedImage, deleteImage } = useContext(GalleryContext);
+
+    return (
         <div id="popup">
-          <button onClick={deleteImage} className='delete-button'>
-            Delete
-          </button>
-            <button onClick={()=>{setSelectedImage("")}} className="close-popup">
+            <button onClick={deleteImage} className='delete-button'>
+                Delete
+            </button>
+            <button onClick={() => { setSelectedImage("") }} className="close-popup">
                 &times;
             </button>
             <div className="popup-container">
                 <img src={selectedImage} alt="" />
             </div>
         </div>
-    )
+    );
 }
-
-// Popup.propTypes = {
-//     zatvoriSlika:PropTypes.func,
-//     izbranaSlika:PropTypes.string
-// }
--- cas9/src/utils/GalleryContext.jsx
+++ cas9/src/utils/GalleryContext.jsx
@@ -2,16 +2,18 @@ import { createContext, useState, useContext } from 'react';
 
 export const GalleryContext = createContext();
 
-export function GalleryProvider({ children }) {
+export function GalleryProvider({ children, initialListOfPhotos }) {
+  const [listOfPhotos, setListOfPhotos] = useState(initialListOfPhotos);
   const [selectedImage, setSelectedImage] = useState('');
 
   const deleteImage = () => {
-    console.log("Delete function called");
+    const updatedList = listOfPhotos.filter(photo => photo.url !== selectedImage);
+    setListOfPhotos(updatedList);
     setSelectedImage('');
   };
 
   return (
-    <GalleryContext.Provider value={{ selectedImage, setSelectedImage, deleteImage }}>
+    <GalleryContext.Provider value={{ selectedImage, setSelectedImage, listOfPhotos, setListOfPhotos, deleteImage }}>
       {children}
     </GalleryContext.Provider>
   );
